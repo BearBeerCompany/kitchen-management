3e1326fcdd8c6cfb0d2d6e5a610f95bde1d7ead2
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-06-14 22:41:35 +0200

    [Project Architecture] * PrimeNG setup

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const {app, BrowserWindow} = require('electron')
 const url = require("url");
 const path = require("path");
 
-let mainWindow
+let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      devTools: false
     }
-  })
+  });
 
   mainWindow.loadURL(
     url.format({
@@ -20,20 +21,19 @@ function createWindow() {
       slashes: true
     })
   );
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
-  })
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
   app.quit()
 })
 
 app.on('activate', function () {
-  if (mainWindow === null) createWindow()
+  if (mainWindow === null)
+    createWindow();
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {PrimeNGConfig} from "primeng/api";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  value: number = 23;
+
+  constructor(private _primengConfig: PrimeNGConfig) {
+    this._primengConfig.ripple = true;
+  }
 }
