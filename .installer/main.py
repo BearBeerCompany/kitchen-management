f4da92150d8e1eed9921253b0a965cda0b13a75f
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-08-01 09:44:00 +0200

    [Installer] * Added progress bar and download status * Implemented tar files extraction after download

--- .installer/main.py
+++ .installer/main.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import requests
+import tarfile
 
 sg.theme("Reddit")
 layout = [
@@ -7,51 +8,82 @@ layout = [
     [[sg.Radio('Windows', "VERSION", default=True)],
      [sg.Radio('Linux', "VERSION", default=False)]],
     [sg.Text("Seleziona la cartella di installazione: ")],
-    [sg.In(size=(25, 1), enable_events=True, key='FOLDER'), sg.FolderBrowse()],
+    [sg.In(size=(70, 1), enable_events=True, key='FOLDER'), sg.FolderBrowse()],
     [sg.Button("Installa", key="OK", size=100, disabled=True)],
-    [sg.Multiline(size=(100, 10), key='OUT_LOG')]
+    [sg.ProgressBar(max_value=10, orientation='h', size=(100, 20), key='PROGRESS')],
+    [sg.Multiline(size=(100, 30), key='OUT_LOG', disabled=True, autoscroll=True)]
 ]
-window = sg.Window('Gestione Piastre Installer', layout, size=(300, 300))
+window = sg.Window('Gestione Piastre Installer', layout, size=(600, 600))
 folder = ""
 log = ""
-release = "https://github.com/bozzelliandrea/kitchen-management/releases/download/v0.0.1-RC1/release.tgz"
+release_detail = "https://api.github.com/repos/bozzelliandrea/kitchen-management/releases/tags/v0.0.1-RC1"
+current_size = 0
+file_total_size = 0
+file_chunk_size = 500000
 
 
-def download_file(url, fil_path):
-    local_filename = fil_path + '/' + url.split('/')[-1]
+def logger(message):
+    global log
+    if log == "":
+        log = message
+    else:
+        log = log + '\n' + message
+    window['OUT_LOG'].update(value=log)
+
+
+def download_file(url, file_path):
+    logger('Starting download file....')
+    local_filename = file_path + '/' + url.split('/')[-1]
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
+            for chunk in r.iter_content(chunk_size=file_chunk_size):
+                update_progress_bar(file_chunk_size)
                 f.write(chunk)
+    logger('Download Complete')
     return local_filename
 
 
-def get_remote_size(url):
-    response = requests.head(url)
-    print('\r\n'.join('{}: {}'.format(k, v) for k, v in response.headers.items()))
-    return response.headers['content-length']
+def get_remote_size_and_url(url):
+    response = requests.get(url)
+    json_object = response.json()
+    return [json_object['assets'][0]['size'], json_object['assets'][0]['browser_download_url']]
 
 
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED:
-        break
-    elif event == "FOLDER":
-        folder = values['FOLDER']
-        if folder is not None:
-            window['OK'].update(disabled=False)
-    elif event == "OK":
-        log = folder
-        window['OUT_LOG'].update(value=log)
-        size = get_remote_size(release)
-        log = log + '\n' + 'File Size: ' + size
-        window['OUT_LOG'].update(value=log)
-        log = log + '\n' + 'Starting download file....'
-        window['OUT_LOG'].update(value=log)
-        local_file = download_file(release)
-        local_file = "bla bla"
-        log = log + '\n' + 'Download Complete' + '\n' + local_file
-        window['OUT_LOG'].update(value=log)
+def update_progress_bar(chunk):
+    global current_size
+    current_size += chunk
+    logger(f'current: {current_size}, total: {file_total_size}')
+    window['PROGRESS'].UpdateBar(current_size, file_total_size)
 
-window.close()
+
+def extract_tar(file_path):
+    logger('Starting file installation')
+    release_tar = tarfile.open(file_path)
+    release_tar.extractall('./gestione_piastre')
+    release_tar.close()
+    logger('Installation Completed!')
+
+
+if __name__ == "__main__":
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == 'FOLDER':
+            folder = values['FOLDER']
+            if folder is not None:
+                window['OK'].update(disabled=False)
+        elif event.startswith('UPDATE_'):
+            logger(f'event: {event}, value: {values[event]}')
+            key_to_update = event[len('UPDATE_'):]
+            window[key_to_update].update(values[event])
+            window.refresh()
+            continue
+        elif event == 'OK':
+            logger(f'Selected folder: {folder}')
+            download_info = get_remote_size_and_url(release_detail)
+            file_total_size = download_info[0]
+            logger(f'File Size: {download_info[0]}' + '\n' + f'Download remote from: {download_info[1]}')
+            local_file = download_file(download_info[1], folder)
+            extract_tar(local_file)
