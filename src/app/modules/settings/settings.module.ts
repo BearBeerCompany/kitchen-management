37c58411c54a1aa0d6ac83c79a195b40e45c88a0
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-08-09 16:44:14 +0200

    [Plate] edit and delete plate

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -3,6 +3,12 @@ import {CommonModule} from '@angular/common';
 import {SettingsComponent} from './settings/settings.component';
 import {RouterModule, Routes} from "@angular/router";
 import {SharedModule} from "../shared/shared.module";
+import {DialogModule} from 'primeng/dialog';
+import {PlateComponentModule} from "../plates/plate/plate.component";
+import {InputTextModule} from "primeng/inputtext";
+import {ReactiveFormsModule} from "@angular/forms";
+import {InputNumberModule} from "primeng/inputnumber";
+import {ColorPickerModule} from "primeng/colorpicker";
 
 const routes: Routes = [
   {
@@ -18,7 +24,13 @@ const routes: Routes = [
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    DialogModule,
+    PlateComponentModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    InputNumberModule,
+    ColorPickerModule
   ]
 })
 export class SettingsModule {
--- src/app/modules/settings/settings/settings.component.ts
+++ src/app/modules/settings/settings/settings.component.ts
@@ -2,8 +2,9 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {I18nService} from "../../../services/i18n.service";
 import {ApiConnector} from "../../../services/api-connector";
 import {Observable} from "rxjs";
-import {Plate} from "../../plates/plate.interface";
+import {Plate, PlateInterface} from "../../plates/plate.interface";
 import {PlateService} from "../../plates/services/plate.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'settings',
@@ -14,10 +15,41 @@ import {PlateService} from "../../plates/services/plate.service";
         <ng-container *ngFor="let plate of plates$ | async">
           <plate-info [config]="plate"
                       (delete)="onDelete($event)"
+                      (edit)="showDialog($event)"
           ></plate-info>
         </ng-container>
       </section>
     </main>
+    <p-dialog header="Modifica Piastra"
+              [style]="{height: '80vh'}"
+              [(visible)]="display"
+              [draggable]="false">
+      <form (ngSubmit)="onSubmit()" [formGroup]="form!">
+        <h3>{{i18n.PLATE.FORM.TITLE}}</h3>
+        <label>{{i18n.PLATE.FORM.NAME}}</label>
+        <input formControlName="name"
+               pInputText
+               type="text">
+        <label>{{i18n.PLATE.FORM.COLOR}}</label>
+        <p-colorPicker [inline]="true"
+                       formControlName="color"></p-colorPicker>
+        <label>{{i18n.PLATE.FORM.NUMBER}}</label>
+        <p-inputNumber [max]="200"
+                       [min]="0"
+                       [showButtons]="true"
+                       formControlName="number"></p-inputNumber>
+        <div id="confirmForm">
+          <button (click)="discardForm()"
+                  [label]="i18n.COMMON.UNDO"
+                  class="p-button-secondary" pButton pRipple
+                  type="button"></button>
+          <button [disabled]="form?.valid ? !form?.valid : true"
+                  [label]="i18n.COMMON.SAVE"
+                  class="p-button-success" pButton pRipple
+                  type="submit"></button>
+        </div>
+      </form>
+    </p-dialog>
   `,
   styleUrls: ['./settings.component.scss']
 })
@@ -25,7 +57,10 @@ export class SettingsComponent implements OnInit {
 
   public readonly i18n: any;
 
+  public display: boolean = false;
+  public selectedPlate?: Plate;
   public plates$: Observable<Plate[]> = new Observable<[]>();
+  public form?: FormGroup | undefined;
 
   constructor(public i18nService: I18nService,
               private _platesService: PlateService) {
@@ -34,11 +69,46 @@ export class SettingsComponent implements OnInit {
 
   ngOnInit(): void {
     this.plates$ = this._platesService.plates$;
+
+    this.form = new FormGroup({
+      name: new FormControl("", Validators.required),
+      color: new FormControl("", Validators.required),
+      number: new FormControl(0, [Validators.required, Validators.pattern("^[0-9]*$")])
+    });
   }
 
   public onDelete(id: string): void {
     this._platesService.delete(id).subscribe(
       () => this._platesService.getAll().subscribe());
+  }
+
+  public onSubmit(): void {
+    this._platesService.update({
+      ...this.form?.value,
+      enabled: true,
+      slot: [0, this.form?.get("number")?.value],
+      id: this.selectedPlate?.id
+    }).subscribe(
+      _ => this._platesService.getAll().subscribe(_ => this.display = false));
+  }
+
+  public discardForm(): void {
+    this.form?.reset();
+    this.display = false;
+  }
+
+  public showDialog(id: string) {
+    this._platesService.getById(id).subscribe(
+      (plate: Plate) => {
+        this.selectedPlate = plate;
+        this.form = new FormGroup({
+          name: new FormControl(plate.name, Validators.required),
+          color: new FormControl(plate.color, Validators.required),
+          number: new FormControl(plate.slot![1], [Validators.required, Validators.pattern("^[0-9]*$")])
+        });
+        this.display = true;
+      }
+    );
   }
 
 }
