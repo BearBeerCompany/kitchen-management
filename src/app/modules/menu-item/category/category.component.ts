4336d1cea99dbb4589d06f8bd60d90ca109468bb
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-08-13 18:03:28 +0200

    feat: add user feedback and confirm modal

--- src/app/modules/menu-item/category/category.component.ts
+++ src/app/modules/menu-item/category/category.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {CategoryService} from "../../plate-menu-items/services/category.service";
 import {Observable, of} from "rxjs";
 import {Category} from "../../plate-menu-items/plate-menu-item";
@@ -8,14 +8,21 @@ import {Category} from "../../plate-menu-items/plate-menu-item";
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnChanges {
 
-  @Input() public categories$: Observable<Category[]> = of([]);
+  @Input() public categories: Category[] = [];
+  @Input() public active?: Category;
 
   @Output() public selected: EventEmitter<Category> = new EventEmitter<Category>(false);
   @Output() public create: EventEmitter<void> = new EventEmitter<void>(false);
 
   public activeCategory?: string;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.categories?.length > 0) {
+      this.onCategoryClick(this.active == null ? this.categories[0] : this.active);
+    }
+  }
 
   public onCategoryClick(category: Category) {
     this.activeCategory = category.id;
