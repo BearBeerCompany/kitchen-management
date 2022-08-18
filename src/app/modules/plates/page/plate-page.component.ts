c855b8ddec4545256932b0d169bd53a520e9f5e2
alberto.rizzi <alberto.rizzi@carel.com>
2022-08-18 10:48:47 +0200

    notifications management in expanded plate

--- src/app/modules/plates/page/plate-page.component.ts
+++ src/app/modules/plates/page/plate-page.component.ts
@@ -1,14 +1,19 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {Subscription} from "rxjs";
+import {config, Observable, Subscription} from "rxjs";
 import {Plate} from "../plate.interface";
 import {PlateService} from "../services/plate.service";
 import {PlateMenuItem} from "../../plate-menu-items/plate-menu-item";
 import {ReactiveQueue} from "../../shared/class/reactive-queue";
+import {PKMINotification, PKMINotificationType} from "../../../services/pkmi-notification";
+import {WebSocketService} from "../../../services/web-socket-service";
+import {MessageService} from "primeng/api";
+import {PlateQueueManagerService} from "../services/plate-queue-manager.service";
 
 @Component({
   selector: 'page',
   template: `
+    <p-toast></p-toast>
     <div class="page-container">
       <plate [config]="config" [queue]="queue"></plate>
     </div>
@@ -32,9 +37,14 @@ export class PlatePageComponent implements OnInit, OnDestroy {
 
   private _subs: Subscription = new Subscription();
   private _id?: string;
+  private _pkmiNotification$: Observable<PKMINotification | null>;
 
   constructor(private _route: ActivatedRoute,
-              private _plateService: PlateService) {
+              private _plateService: PlateService,
+              private _plateQueueManagerService: PlateQueueManagerService,
+              private _webSocketService: WebSocketService,
+              private _messageService: MessageService) {
+    this._pkmiNotification$ = this._webSocketService.pkmiNotifications$;
   }
 
   public ngOnInit(): void {
@@ -53,11 +63,60 @@ export class PlatePageComponent implements OnInit, OnDestroy {
             this.queue.refresh()
           }
         })
-      }));
+      })
+    );
+
+    this._subs.add(
+      this._pkmiNotification$.subscribe((notification: PKMINotification | null) => {
+        console.log('plate page notification: ' + notification?.type);
+        if (notification) {
+          const msgData = WebSocketService.getNotificationMsgData(notification);
+          this._messageService.add({
+            severity: msgData.severity,
+            summary: msgData.summary,
+            detail: msgData.detail,
+            life: 3000
+          });
+
+          switch (notification.type) {
+            case PKMINotificationType.PKMI_ADD:
+            case PKMINotificationType.PKMI_ADD_ALL:
+              this._refreshPlateQueue();
+              break;
+            case PKMINotificationType.PKMI_UPDATE:
+              this._refreshPlateQueue();
+              break;
+            case PKMINotificationType.PKMI_UPDATE_ALL:
+              // todo
+              break;
+            case PKMINotificationType.PKMI_DELETE:
+              // todo
+              break;
+            case PKMINotificationType.PKMI_DELETE_ALL:
+              // todo
+              break;
+          }
+        }
+      })
+    );
   }
 
   public ngOnDestroy(): void {
     this._subs.unsubscribe();
+  }
+
+  private _refreshPlateQueue() {
+    this._subs.add(
+      this._plateService.getById(this.config.id!).subscribe(plate => {
+        this.config = { ...config, ...plate};
+        // refresh plate queue
+        this._plateService.getStatusById(this.config.id!).subscribe(items => {
+          // let queue = this._plateQueueManagerService.getQueue(this.config.id!);
+          this.queue.values = items;
+          this.queue.refresh();
+        })
+      })
+    );
   }
 
 }
